13dfbee8add07f7ab907ca6453dc1aebf3095405
Aman Garg <aman.garg@idemia.com>
2020-08-17 13:46:45 +0530

    Integer to Roman and vice versa

--- src/main/java/data_structures/arrays/ConvertIntegerToRoman.java
+++ src/main/java/data_structures/arrays/ConvertIntegerToRoman.java
@@ -1,6 +1,22 @@
 package data_structures.arrays;
 
-// TODO
 public class ConvertIntegerToRoman {
+
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+
+    public static void main(String[] args) {
+        ConvertIntegerToRoman convertIntegerToRoman = new ConvertIntegerToRoman();
+        System.out.println(convertIntegerToRoman.convertIntegerToRoman(9));
+    }
+
+    private String convertIntegerToRoman(int i) {
+        return thousands[i/1000] + hundreds[(i%1000)/100] +
+                tens[(i%100)/10] + units[i%10];
+    }
+
 
 }
--- src/main/java/data_structures/arrays/ConvertRomanToInteger.java
+++ src/main/java/data_structures/arrays/ConvertRomanToInteger.java
@@ -1,6 +1,42 @@
 package data_structures.arrays;
 
-// TODO
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConvertRomanToInteger {
+    private static final Map<Character, Integer> map = new HashMap<>();
+
+    static  {
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertRomanToInteger("XXX"));
+    }
+
+    private static int convertRomanToInteger(String s) {
+        int result = getValueForIndex(s, 0);
+
+        for (int i = 1; i< s.length(); i++) {
+            int currentValue  = getValueForIndex(s, i);
+            result += currentValue;
+            int previousValue  = getValueForIndex(s, i-1);
+            if(currentValue > previousValue) {
+                result = result - 2*previousValue;
+            }
+        }
+
+        return result;
+    }
+
+    private static int getValueForIndex(String str, int index ) {
+        return map.get(str.charAt(index));
+    }
 
 }
