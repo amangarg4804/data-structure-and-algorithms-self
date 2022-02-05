5e47768a9c6697e9aab43bd948bb38e17ce9c8f4
Aman Garg <aman.garg1@ihsmarkit.com>
2022-02-05 10:19:56 +0530

    Middle of linked list

---
+++ src/main/java/algorithms/patterns/fastslowpointers/MiddleOfLinkedList.java
@@ -0,0 +1,29 @@
+package algorithms.patterns.fastslowpointers;
+
+public class MiddleOfLinkedList {
+
+  public static void main(String[] args) {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    head.next.next.next.next = new ListNode(5);
+
+    System.out.println("Middle Node :" + middleNode(head).value);
+    head.next.next.next.next.next = new ListNode(6);
+    System.out.println("Middle Node :" + middleNode(head).value);
+    head.next.next.next.next.next.next = new ListNode(7);
+    System.out.println("Middle Node :" + middleNode(head).value);
+  }
+
+  public static ListNode middleNode(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast!=null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+
+}
