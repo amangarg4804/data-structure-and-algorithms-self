d13383e23d945e20fd544acbb7547a5b78fdb0fc
Aman Garg <aman.garg1@ihsmarkit.com>
2022-02-27 09:11:49 +0530

    Connect all level order siblings

---
+++ src/main/java/algorithms/patterns/bfs/ConnectAllLevelOrderSiblings.java
@@ -0,0 +1,51 @@
+package algorithms.patterns.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ConnectAllLevelOrderSiblings {
+
+  public static void main(String[] args) {
+    TreeNodeSibling root = new TreeNodeSibling(12);
+    root.left = new TreeNodeSibling(7);
+    root.right = new TreeNodeSibling(1);
+    root.left.left = new TreeNodeSibling(9);
+    root.right.left = new TreeNodeSibling(10);
+    root.right.right = new TreeNodeSibling(5);
+    connectSiblings(root);
+    print(root);
+  }
+
+  public static void connectSiblings(TreeNodeSibling root) {
+    if(root==null) {
+      return;
+    }
+    Queue<TreeNodeSibling> queue = new LinkedList<>();
+    queue.offer(root);
+    TreeNodeSibling previous = null;
+    while (!queue.isEmpty()) {
+      for(int i = 0 ; i< queue.size(); i++) {
+        TreeNodeSibling current = queue.poll();
+        if(previous!= null) {
+          previous.next = current;
+        }
+        previous = current;
+        if(current.left != null) {
+          queue.add(current.left);
+        }
+        if(current.right != null) {
+          queue.add(current.right);
+        }
+      }
+    }
+    System.out.println("hello");
+  }
+
+  public static void print(TreeNodeSibling root) {
+    TreeNodeSibling current = root;
+    while(current !=null) {
+      System.out.print(current.value + " ");
+      current = current.next;
+    }
+  }
+}
