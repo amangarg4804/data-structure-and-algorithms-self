f6d94a6221ab275a1a108c6bab4b7e31bbb6c650
Aman Garg <aman.garg1@ihsmarkit.com>
2022-03-06 17:09:01 +0530

    Path sum iterative

--- src/main/java/algorithms/patterns/dfs/PathSum.java
+++ src/main/java/algorithms/patterns/dfs/PathSum.java
@@ -1,4 +1,7 @@
 package algorithms.patterns.dfs;
+
+import java.util.Stack;
+
 // Given a binary tree and a number ‘S’,
 // find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals ‘S’.
 public class PathSum {
@@ -12,16 +15,47 @@ public class PathSum {
     root.right.right = new TreeNode(5);
     System.out.println(hasPathWithSum(root, 23));
     System.out.println(hasPathWithSum(root, 16));
+    System.out.println(hasPathWithSumIterative(root, 23));
+    System.out.println(hasPathWithSumIterative(root, 16));
   }
 
   public static boolean hasPathWithSum(TreeNode root, int sum) {
-    if (root == null) {
+  if(root ==null) {
+    return false;
+  }
+
+  if(root.left==null && root.right==null && root.value == sum) {
+    return true;
+  }
+  return hasPathWithSum(root.left, sum - root.value) || hasPathWithSum(root.right,
+        sum - root.value);
+  }
+
+  public static boolean hasPathWithSumIterative(TreeNode root, int sum) {
+    if(root ==null) {
       return false;
     }
-    if (root.left == null && root.right == null && root.value == sum) {
-      return true;
+    Stack<TreeNode> nodeStack = new Stack<>();
+    Stack<Integer> sumStack = new Stack<>();
+    nodeStack.push(root);
+    sumStack.push(sum- root.value);
+    while (!nodeStack.isEmpty()) {
+      TreeNode currentNode = nodeStack.pop();
+      Integer stackTop = sumStack.pop();
+      if(currentNode.left ==null && currentNode.right==null && stackTop.equals(0)) {
+        return true;
+      }
+      if(currentNode.left != null) {
+        nodeStack.push(currentNode.left);
+        sumStack.push(stackTop-currentNode.left.value);
+      }
+      if(currentNode.right != null) {
+        nodeStack.push(currentNode.right);
+        sumStack.push(stackTop-currentNode.right.value);
+      }
+
     }
-    return hasPathWithSum(root.left, sum - root.value) || hasPathWithSum(root.right,
-        sum - root.value);
+
+    return false;
   }
 }
---
+++ src/main/java/util/TestStartWith.java
@@ -0,0 +1,9 @@
+package util;
+
+public class TestStartWith {
+  public static void main(String[] args) {
+    System.out.println("abc".startsWith(null));
+    Long abc = 1L;
+    System.out.println(abc.toString());
+  }
+}
