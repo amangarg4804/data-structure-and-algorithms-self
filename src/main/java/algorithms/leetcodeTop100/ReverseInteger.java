e16543d43c9ed31cfd50bbee55ab0bd55d5635b6
Aman Garg <aman.garg1@ihsmarkit.com>
2022-05-01 12:04:09 +0530

    Reverse Integer

---
+++ src/main/java/algorithms/leetcodeTop100/ReverseInteger.java
@@ -0,0 +1,23 @@
+package algorithms.leetcodeTop100;
+
+public class ReverseInteger {
+  public int reverse(int x) {
+    int reversed = 0;
+    while (x != 0) {
+
+
+      int digit = x %10;
+      x = x/10;
+      if(reversed >Integer.MAX_VALUE/10  ||  reversed==Integer.MAX_VALUE/10 && digit >7)  {
+        return 0;
+      }
+      if(reversed < Integer.MIN_VALUE/10  ||  reversed==Integer.MIN_VALUE/10 && digit < -8)  {
+        return 0;
+      }
+      reversed *=10;
+      reversed +=digit;
+
+    }
+    return reversed;
+  }
+}
