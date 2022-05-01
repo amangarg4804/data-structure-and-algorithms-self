0530a64e22f578a9577e3111b939a4dc14ce2463
Aman Garg <aman.garg1@ihsmarkit.com>
2022-05-01 11:26:42 +0530

    Zigzag conversation method 2

--- src/main/java/algorithms/leetcodeTop100/ZigzagConversion.java
+++ src/main/java/algorithms/leetcodeTop100/ZigzagConversion.java
@@ -6,20 +6,52 @@ public class ZigzagConversion {
     System.out.println(convert("PAYPALISHIRING", 3));
     System.out.println(convert("PAYPALISHIRING", 4));
   }
+
   public static String convert(String s, int numRows) {
-    if(numRows ==0) {
+    // time O(n^2)
+    if (numRows == 0) {
       return s;
     }
     StringBuilder sb = new StringBuilder();
-    for(int i =0 ; i< numRows; i++ ) {
-      int increment  = numRows*2 -2;
-      for(int j = i; j< s.length(); j += increment) {
+    for (int i = 0; i < numRows; i++) {
+      int increment = numRows * 2 - 2;
+      for (int j = i; j < s.length(); j += increment) {
         sb.append(s.charAt(j));
-        if(i >0 && i< numRows -1 && j + increment -(2*i) < s.length()) {
-          sb.append(s.charAt(j + increment -(2*i) ));
+        if (i > 0 && i < numRows - 1 && j + increment - (2 * i) < s.length()) {
+          sb.append(s.charAt(j + increment - (2 * i)));
         }
       }
     }
     return sb.toString();
+  }
+
+  public String convertMethod2(String s, int numRows) {
+    // time O(n)
+    if (numRows == 0) {
+      return s;
+    }
+    StringBuilder[] sb = new StringBuilder[numRows];
+
+    for (int i = 0; i < numRows; i++) {
+      sb[i] = new StringBuilder();
+    }
+
+    int index = 0;
+    while (index < s.length()) {
+      // down
+      for (int j = 0; j < numRows && index < s.length(); j++) {
+        sb[j].append(s.charAt(index++));
+      }
+      // up excluding zero
+      for (int j = numRows - 2; j > 0 && index < s.length(); j--) {
+        sb[j].append(s.charAt(index++));
+      }
+    }
+
+    StringBuilder result = new StringBuilder();
+    for (StringBuilder each : sb) {
+      result.append(each);
+    }
+    return result.toString();
   }
 }
