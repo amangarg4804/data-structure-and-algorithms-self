2e6116d51551b363301d2df58c20dcd592db9cb6
Aman Garg <aman.garg1@ihsmarkit.com>
2022-05-02 11:00:55 +0530

    Atoi

---
+++ src/main/java/algorithms/leetcodeTop100/Atoi.java
@@ -0,0 +1,42 @@
+package algorithms.leetcodeTop100;
+
+// String to integer
+public class Atoi {
+
+  public int myAtoi(String s) {
+    if (s == null || s.equals("")) {
+      return 0;
+    }
+    int i = 0;
+    int sign = 1;
+    while (i < s.length() && s.charAt(i) == ' ') {
+      i++;
+    }
+    System.out.println(i + " " + s.length());
+    if (i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
+      if (s.charAt(i) == '-') {
+        sign = -1;
+      }
+      i++;
+    }
+    while (i < s.length() && s.charAt(i) == '0') {
+      i++;
+    }
+
+    if (i >= s.length()) {
+      return 0;
+    }
+    int result = 0;
+    while (i < s.length() && Character.isDigit(s.charAt(i))) {
+      int currentInt = s.charAt(i) - '0';
+      if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10
+          && currentInt > Integer.MAX_VALUE % 10)) {
+        return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+      }
+      result = result * 10 + currentInt;
+      i++;
+    }
+    return result * sign;
+
+  }
+}
