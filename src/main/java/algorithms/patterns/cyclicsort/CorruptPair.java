4a6d1a582777d1e42201074a9d21eca07f5c7c67
Aman Garg <aman.garg1@ihsmarkit.com>
2022-05-03 10:19:00 +0530

    Corrupt pair cleaner solution

--- src/main/java/algorithms/patterns/cyclicsort/CorruptPair.java
+++ src/main/java/algorithms/patterns/cyclicsort/CorruptPair.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class CorruptPair {
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(findNumbers(new int[]{3, 1, 2, 5, 2})));
-    System.out.println(Arrays.toString(findNumbers(new int[]{3, 1, 2, 3, 6, 4})));
+    System.out.println(Arrays.toString(corruptPair(new int[]{3, 1, 2, 5, 2})));
+    System.out.println(Arrays.toString(corruptPair(new int[]{3, 1, 2, 3, 6, 4})));
   }
 
   public static int[] findNumbers(int[] nums) {
@@ -32,6 +32,25 @@ public class CorruptPair {
       }
     }
     return new int[]{duplicate, missing};
+  }
+
+  public static int[] corruptPair(int[] nums) {
+    int i = 0;
+    while (i < nums.length) {
+      int j = nums[i] - 1;
+      if (nums[i] != nums[j]) {
+        swap(nums, i, j);
+      } else {
+        i++;
+      }
+    }
+
+    for (int j = 0; j < nums.length; j++) {
+      if (nums[j] != j + 1) {
+        return new int[]{nums[j], j + 1};
+      }
+    }
+    return new int[]{-1, -1};
   }
 
   private static void swap(int[] nums, int i, int j) {
--- src/main/java/algorithms/patterns/cyclicsort/SmallestMissingPositiveNumber.java
+++ src/main/java/algorithms/patterns/cyclicsort/SmallestMissingPositiveNumber.java
@@ -3,25 +3,25 @@ package algorithms.patterns.cyclicsort;
 public class SmallestMissingPositiveNumber {
 
   public static void main(String[] args) {
-    System.out.println(findNumber(new int[] {-3, 1, 5, 4, 2}));
-    System.out.println(findNumber(new int[] {3, -2, 0, 1, 2}));
-    System.out.println(findNumber(new int[] {3, 2, 5, 1}));
+    System.out.println(findNumber(new int[]{-3, 1, 5, 4, 2}));
+    System.out.println(findNumber(new int[]{3, -2, 0, 1, 2}));
+    System.out.println(findNumber(new int[]{3, 2, 5, 1}));
   }
 
   public static int findNumber(int[] nums) {
-    int i=0;
-    while(i< nums.length) {
-      int j = nums[i] -1;
-      if(j >= 0  && j < nums.length && nums[i] != nums[j] ) {
+    int i = 0;
+    while (i < nums.length) {
+      int j = nums[i] - 1;
+      if (j >= 0 && j < nums.length && nums[i] != nums[j]) {
         swap(nums, i, j);
       } else {
         i++;
       }
     }
 
-    for(i = 0; i< nums.length ; i++) {
-      if(nums[i]-1 !=i) {
-        return i+1;
+    for (i = 0; i < nums.length; i++) {
+      if (nums[i] != i + 1) {
+        return i + 1;
       }
     }
     return -1;
