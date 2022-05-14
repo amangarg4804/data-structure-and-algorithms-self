ed2fca0262b34bfc51d073c66e09f6dd7824ff0a
Aman Garg <aman.garg1@ihsmarkit.com>
2022-05-14 11:53:00 +0530

    Rotation count

--- src/main/java/algorithms/patterns/binarysearch/RotationCount.java
+++ src/main/java/algorithms/patterns/binarysearch/RotationCount.java
@@ -10,11 +10,20 @@ public class RotationCount {
 
   private static int count (int [] arr) {
     // 10, 15, 1, 3, 8
+    // time O(logN), space O(1)
+    if(arr[0] < arr[arr.length-1]) {
+      return 0;
+    }
     int start = 0;
     int end = arr.length -1;
     while (start < end) {
       int mid = start + (end- start)/2;
-
+      if(mid +1 < arr. length && arr[mid] > arr[mid+1]) {
+        return mid+1;
+      }
+      if(mid-1 >=0 && arr[mid-1] > arr[mid]) {
+        return mid;
+      }
       if(arr[start] < arr[mid]) {
         start = mid + 1;
       } else {
