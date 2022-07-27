49464e1d146c4ca4fbf542f9292dbf47879127ce
Aman Garg <aman.garg1@ihsmarkit.com>
2022-07-27 10:25:30 +0530

    Fix triplet sum close to target

--- src/main/java/algorithms/patterns/twopointers/TripletSumCloseToTarget.java
+++ src/main/java/algorithms/patterns/twopointers/TripletSumCloseToTarget.java
@@ -9,45 +9,30 @@ public class TripletSumCloseToTarget {
     System.out.println(tripletSum(new int[]{-3, -1, 1, 2}, 1));
     System.out.println(tripletSum(new int[]{1, 0, 1, 1}, 100));
 
-    System.out.println(tripletSum(new int[]{-1,2,1,-4}, 1));
+    System.out.println(tripletSum(new int[]{-1, 2, 1, -4}, 1));
   }
 
   static int tripletSum(int[] arr, int targetSum) {
     int smallestDiff = Integer.MAX_VALUE;
-    int tripletSum = Integer.MAX_VALUE;
     Arrays.sort(arr);
-
     for (int i = 0; i < arr.length - 2; i++) {
-      int closestSumWithCurrentIndex = tripletSum(arr,    i, tripletSum, smallestDiff, targetSum);
-      int diff = Math.min(Math.abs(closestSumWithCurrentIndex - targetSum),
-          Math.abs(targetSum - closestSumWithCurrentIndex));
-      if (diff < smallestDiff) {
-        smallestDiff = diff;
-        tripletSum = closestSumWithCurrentIndex;
-      } else if (diff == smallestDiff) {
-        tripletSum = Math.min(tripletSum, closestSumWithCurrentIndex);
-
+      int left = i + 1;
+      int right = arr.length - 1;
+      while (left < right) {
+        int currentDiff = targetSum - arr[i] - arr[left] - arr[right];
+        if (currentDiff == 0) {
+          return targetSum - currentDiff; // triplet with exact sum
+        }
+        if (Math.abs(currentDiff) < Math.abs(smallestDiff)) {
+          smallestDiff = currentDiff;
+        }
+        if (currentDiff > 0) {
+          left++;
+        } else {
+          right--;
+        }
       }
     }
-    return tripletSum;
-
-  }
-
-  private static int tripletSum(int[] arr, int currentIndex, int tripletSum, int smallestDiff,
-      int targetSum) {
-    int left = currentIndex + 1;
-    int right = currentIndex + 2;
-    while (right < arr.length) {
-      int diff = Math.abs(targetSum - arr[currentIndex] - arr[left] - arr[right]);
-      if (diff <= smallestDiff) {
-        smallestDiff = diff;
-        tripletSum = arr[currentIndex] + arr[left] + arr[right];
-      } else {
-        break;
-      }
-      right++;
-      left++;
-    }
-    return tripletSum;
+    return targetSum - smallestDiff;
   }
 }
