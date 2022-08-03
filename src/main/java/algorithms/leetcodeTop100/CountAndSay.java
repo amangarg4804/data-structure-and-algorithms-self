f0161a5cb441a1a5251dc5db259c67405fabed30
Aman Garg <aman.garg1@ihsmarkit.com>
2022-08-03 16:15:42 +0530

    Count And Say

--- src/main/java/algorithms/leetcodeTop100/CountAndSay.java
+++ src/main/java/algorithms/leetcodeTop100/CountAndSay.java
@@ -1,22 +1,23 @@
 package algorithms.leetcodeTop100;
 
 public class CountAndSay {
+
   public String countAndSay(int n) {
-    if(n==1) {
+    if (n == 1) {
       return "1";
     }
-    String s = countAndSay(n-1);
+    String s = countAndSay(n - 1);
     int nextIndex = 1;
-    int currentNumber = s.charAt(0) -'0';
-    int count =1;
+    int currentNumber = s.charAt(0) - '0';
+    int count = 1;
     StringBuilder sb = new StringBuilder();
     while (nextIndex < s.length()) {
-      if(s.charAt(nextIndex)-'0' == currentNumber) {
+      if (s.charAt(nextIndex) - '0' == currentNumber) {
         count++;
       } else {
         sb.append(count).append(currentNumber);
         currentNumber = s.charAt(nextIndex) - '0';
-        count =1;
+        count = 1;
       }
       nextIndex++;
     }
