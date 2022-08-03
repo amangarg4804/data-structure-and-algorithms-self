2ec6de8c1c7f55c88f498541bff9abcf0f5c57d4
Aman Garg <aman.garg1@ihsmarkit.com>
2022-08-03 17:06:24 +0530

    Combination Sum

---
+++ src/main/java/algorithms/leetcodeTop100/CombinationSum.java
@@ -0,0 +1,33 @@
+package algorithms.leetcodeTop100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> resultList = new ArrayList<>();
+    combinationSum(0, candidates, target, resultList, new ArrayList<>());
+    return resultList;
+  }
+
+  private void combinationSum(int currentIndex, int[] candidates, int target,
+      List<List<Integer>> resultList, ArrayList<Integer> currentList) {
+    if (target == 0 || currentIndex == candidates.length) {
+      if (target == 0) {
+        resultList.add( new ArrayList<>(currentList));
+      }
+    }
+    //  accept currentIndex
+    if (candidates[currentIndex] <= target) {
+      currentList.add(candidates[currentIndex]);
+      combinationSum(currentIndex, candidates, target - candidates[currentIndex], resultList,
+          currentList);
+      currentList.remove(currentList.size() - 1); // to backtrack
+    }
+    //  reject currentIndex
+    combinationSum(currentIndex + 1, candidates, target - candidates[currentIndex + 1], resultList,
+        currentList);
+  }
+
+}
