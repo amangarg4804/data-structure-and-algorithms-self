ef7e2ee2ffd32f516e4c91da232121ce432db5b8
B0282234 <b0282234@APB-LTB0282234-MAC.local>
2022-09-11 10:56:18 +0530

    Fibonacci dp bottom up

---
+++ src/main/java/algorithms/leetcodeTop100/JumpGameII.java
@@ -0,0 +1,13 @@
+package algorithms.leetcodeTop100;
+
+public class JumpGameII {
+    // TODO:
+    public int jump(int[] nums) {
+//        int[] dp = new int[nums.length];
+//        dp[nums.length-1] =0;
+//        for(int i= nums.length-2; i>=0; i--) {
+//            dp[i] = Math.min()
+//        }
+        return 0;
+    }
+}
