ce929c6c0219aa7daca56fe2a1f065c385e3fe07
Aman Garg <>
2022-09-11 14:33:48 +0530

    Tree preorder traversal recursive

---
+++ src/main/java/algorithms/leetcodetree/PreOrderTraversal.java
@@ -0,0 +1,21 @@
+package algorithms.leetcodetree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreOrderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderTraversal(root, result);
+        return result;
+    }
+
+    private void preorderTraversal(TreeNode root, List<Integer> result) {
+        if(root==null) {
+            return;
+        }
+        result.add(root.val);
+        preorderTraversal(root.left, result);
+        preorderTraversal(root.right, result);
+    }
+}
