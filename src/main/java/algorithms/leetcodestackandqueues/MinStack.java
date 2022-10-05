5648a52fb2cc5b968bcf15a7245e931fab00d466
Aman Garg <>
2022-10-05 11:17:28 +0530

    Minimum Stack

---
+++ src/main/java/algorithms/leetcodestackandqueues/MinStack.java
@@ -0,0 +1,42 @@
+package algorithms.leetcodestackandqueues;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class MinStack {
+    List<Node> list = new ArrayList<>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if(list.size() !=0) {
+            list.add(new Node(val, Math.min(getMin(), val)));
+        } else {
+            list.add(new Node(val, val));
+        }
+    }
+
+    public void pop() {
+        list.remove(list.size()-1);
+    }
+
+    public int top() {
+        return list.get(list.size()-1).val;
+    }
+
+    public int getMin() {
+        return list.get(list.size()-1).min;
+    }
+}
+
+class Node {
+    int val;
+    int min;
+
+    Node(int val, int min) {
+        this.val = val;
+        this.min = min;
+    }
+}
