247207b0af4d9ba96a11acf82a9f562dbfca0a82
Aman Garg <>
2022-11-24 13:19:39 +0530

    BST Iterator

---
+++ src/main/java/algorithms/bst/BSTIterator.java
@@ -0,0 +1,52 @@
+package algorithms.bst;
+
+import java.util.*;
+
+// Time complexity O(1), space O(n) for queue, n is no of nodes in tree
+public class BSTIterator {
+    Queue<Integer> q = new LinkedList<>();
+    public BSTIterator(TreeNode root) {
+        //assuming tree has atleast one node
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while(current!=null || !stack.isEmpty()) {
+            while (current!=null) {
+                stack.push(current);
+                current = current.left;
+            }
+            if(!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                q.offer(node.val);
+                current = node.right;
+            }
+        }
+    }
+
+    public int next() {
+        // assuming next is called on non-empty queue
+        return q.poll();
+    }
+
+    public boolean hasNext() {
+        return !q.isEmpty();
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/main/java/algorithms/bst/BSTIterator2.java
@@ -0,0 +1,54 @@
+package algorithms.bst;
+
+import java.util.Stack;
+
+// Time complexity of next() is amortized O(1). Whenever a node on top of stack contains right node, we will traverse till we reach the deepest left node of the right node
+// Space used is O(h), at any point, stack will contain elements equal to height
+public class BSTIterator2 {
+    Stack<TreeNode> stack;
+
+    public BSTIterator2(TreeNode root) {
+        //assuming tree has at least one node
+        stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
+        }
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+        if (node.right != null) {
+            TreeNode current = node.right;
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+        }
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
