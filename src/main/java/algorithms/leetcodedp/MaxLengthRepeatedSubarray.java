d68aa9e14282b8cfb7505a0e8604e90e046b1bdc
Aman Garg <aman@L-TDQWT9DP7Q.local>
2023-12-26 10:12:06 +0530

    dp problems. Bottom up-max length repeated subarray

--- src/main/java/algorithms/leetcodedp/MaxLengthRepeatedSubarray.java
+++ src/main/java/algorithms/leetcodedp/MaxLengthRepeatedSubarray.java
@@ -70,5 +70,39 @@ public class MaxLengthRepeatedSubarray {
         }
         dp[i][j] = 1+ dp(nums1, nums2, i+1, j+1, dp);
         return dp[i][j];
+        //The function dp is called for each pair of indices (i, j) in the nested loops of the findLength function.
+        // In the worst case, the function dp is called for every combination of i and j. The recursion explores all possible pairs of indices in nums1 and nums2.
+        //
+        //The time complexity of this algorithm is O(m * n * min(m, n)), where m is the length of nums1 and n is the length of nums2.
+        // This is because there are m * n unique pairs of indices (i, j) to explore, and for each pair, the recursion may take up to min(m, n)
+        // steps to reach the base case.
+        //
+        //The space complexity is also O(m * n) due to the 2D array dp, which is used for memoization.
     }
+
+    public int findLength3(int[] nums1, int[] nums2) {
+        int max = Integer.MIN_VALUE;
+        // arrays have same length
+        // i and j vary so two dimensional dp
+        int[][] dp = new int[nums1.length+1][nums2.length+1];
+
+        for(int i=nums1.length-1; i >= 0; i--) {
+            for(int j=nums2.length-1; j >=0; j--) {
+                // 0 1 2
+                // 1 2 3
+                // 3 2 3
+                // dp[2][2]=1, dp[2][1] =0, dp[2][0] = 1
+                // dp[1][2]=0, dp[1][1] = 2
+                if(nums1[i] ==nums2[j]) {
+                    dp[i][j] = dp[i+1][j+1] +1;
+                }
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max;
+        //time complexity is O(m * n), where m =length of nums1 and n=length of nums2.
+    }
+
+
+
 }
